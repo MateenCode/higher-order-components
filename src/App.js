197c2bd5a7f90af71e08919739d6d5783d1d94db
Mateen Kazia <mateencode@gmail.com>
2020-02-03 15:43:48 -0600

    setting up basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import UserProfile from "./components/user/user-profile.component";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <UserProfile name='mateen' email='mateenkazia@gmail.com' />
+    </div>
+  );
 }
 
 export default App;
