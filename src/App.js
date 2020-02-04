ada918a9eff0ae67bb1ac51aea648ebb9a063f89
Mateen Kazia <mateencode@gmail.com>
2020-02-03 18:09:58 -0600

    user list is  displaying users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-import UserProfile from "./components/user/user-profile.component";
+import UserProfile from "./components/user-profile/user-profile.component";
+import UserList from "./components/user-list/user-list.component";
 
 function App() {
   return (
     <div className='App'>
+      <UserList />
       <UserProfile name='mateen' email='mateenkazia@gmail.com' />
     </div>
   );
---
+++ src/components/user-list/user-list.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class UserList extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response =>
+      response.json().then(data => this.setState({ users: data.slice(0, 3) }))
+    );
+  }
+  render() {
+    return (
+      <div className='container user-list'>
+        <h1> User List</h1>
+        {this.state.users.map(user => (
+          <div className='post' key={user.id}>
+            <h1> {user.name}</h1>
+            <h2>{user.email}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default UserList;
